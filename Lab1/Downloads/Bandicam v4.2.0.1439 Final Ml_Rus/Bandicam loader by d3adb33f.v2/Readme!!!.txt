e4f3a20df4de3d4710676a3c3ba09f7eaf99cad2
Alexandr Kvasnikov <uselessflower@gmail.com>
2018-12-11 14:51:52 +0300

    heyhey
    hey dude

--- Lab1/Downloads/Bandicam v4.2.0.1439 Final Ml_Rus/Bandicam loader by d3adb33f.v2/Readme!!!.txt
+++
@@ -1,4 +0,0 @@
-﻿1. Install bandicam (do not execute it)
-2. Register bandicam with keygen
-3. Execute loader
---------------------------------
--- lab4/Database1/Database1/SqlTrigger1.cs
+++ lab4/Database1/Database1/SqlTrigger1.cs
@@ -9,7 +9,18 @@ public partial class Triggers
     // [Microsoft.SqlServer.Server.SqlTrigger (Name="SqlTrigger1", Target="Table1", Event="FOR UPDATE")]
     public static void SqlTrigger1 ()
     {
-	    SqlContext.Pipe.Send("Trigger. Just Trigger. And it works. ");
+        SqlContext.Pipe.Send("Trigger. Just Trigger. And it works. ");
+
+        using (SqlConnection c = new SqlConnection("context connection = true"))
+        {
+            SqlCommand cmd = new SqlCommand(
+                "select GroupId, Name " +
+                "from Deleted ", c);
+            c.Open();
+            SqlContext.Pipe.ExecuteAndSend(cmd);
+            c.Close();
+        }
     }
 }
 
+//вывести, что удаляется
